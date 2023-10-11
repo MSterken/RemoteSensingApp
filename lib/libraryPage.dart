446ff9aa7122a731568010b3caa4f79e25374359
Max <485736@student.saxion.nl>
2023-10-11 13:01:14 +0200

    navigation bar now leads to different pages

---
+++ lib/libraryPage.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+class libraryPage extends StatefulWidget{
+
+  @override
+  _libraryPageState createState() => _libraryPageState();
+
+}
+class _libraryPageState extends State {
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    body: const Center(
+      child: Text("hier moet iets"),
+    ),
+  );
+  }
+}
+
+
---
+++ lib/mapPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class mapPage extends StatefulWidget{
+
+  @override
+  _mapPageState createState() => _mapPageState();
+
+}
+class _mapPageState extends State {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text("hier moet ook iets"),
+      ),
+    );
+  }
+}
