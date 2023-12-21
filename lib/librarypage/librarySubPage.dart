4a9c990947599588c0ee810c7b8cf33ddc90ee6c
Max <485736@student.saxion.nl>
2023-12-21 12:15:40 +0100

    informatie voor LiDAR en Luchtfoto's toegevoegd

--- lib/librarypage/librarySubPage.dart
+++ lib/librarypage/librarySubPage.dart
@@ -26,19 +26,22 @@ class _LibrarySubPageState extends State<LibrarySubPage> {
         title: Text(minor.subTitle),
       ),
       body: Container(
-        child: ListView.builder(
-            itemCount: minor.paragraphs.length ,
-            itemBuilder: (BuildContext context, int index){
-              return Container(
-                  child : Column(
-                    children: [
-                      Text(minor.paragraphs.elementAt(index).text),
-                      if (minor.paragraphs.elementAt(index).imagePath != "")
-                        Image(image: AssetImage("assets/images/${minor.paragraphs.elementAt(index).imagePath}"))
-                    ],
-                  )
-              );
-            }
+        width: double.maxFinite,
+        child: Expanded (
+          child: ListView.builder(
+              itemCount: minor.paragraphs.length ,
+              itemBuilder: (BuildContext context, int index){
+                return Container(
+                    child : Column(
+                      children: [
+                        Text(minor.paragraphs.elementAt(index).text),
+                        if (minor.paragraphs.elementAt(index).imagePath != "")
+                          Image(image: AssetImage("assets/images/${minor.paragraphs.elementAt(index).imagePath}"))
+                      ],
+                    )
+                );
+              }
+          ),
         ),
       ),
     );
